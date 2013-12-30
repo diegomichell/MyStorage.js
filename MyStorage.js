1d062e91eb3671780e7292da3fbb84e6af1c48a1
Diego Perez <ivanevil31@gmail.com>
2013-12-30 00:36:55 -0400

    Making it public, Note:Doc is missing

---
+++ MyStorage.js
@@ -0,0 +1,35 @@
+/**
+*	libName:MyStorage.js
+*	Author:Diego Ivan Perez Michel (ivanevi)
+*		
+*	Description:	
+*	
+*	MyStorage.js allows you to save any kind of Object in a "localStorage" or "sessionStorage" not just Strings. 
+*/
+
+Storage.prototype.setObject = function(key,object){
+    this.setItem(key,JSON.stringify(object));
+}
+
+Storage.prototype.getObject = function(key) {
+	var value = this.getItem(key);
+	return JSON.parse(value);
+};
+
+Storage.prototype.setInt = function(key,integer){
+    this.setItem(key,integer);
+}
+
+Storage.prototype.getInt = function(key) {
+	var value = this.getItem(key);
+	return parseInt(value);
+};
+
+Storage.prototype.setFloat = function(key,float){
+    this.setItem(key,float);
+}
+
+Storage.prototype.getFloat = function(key) {
+	var value = this.getItem(key);
+	return parseFloat(value);
+};
